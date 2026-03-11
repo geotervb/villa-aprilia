ffc67431223fa64d3c170cbfa3225d1475f11ff4
geotervb <stefano.baldissera@geoter.verbania.it>
2026-03-11 17:59:51 +0100

    aggiunta point cloud Alpe Selle

---
+++ pointclouds/alpe_selle/cloud.js
@@ -0,0 +1,59 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 13675703,
+    "boundingBox": {
+        "lx": 430143.988319397,
+        "ly": 5086824.913887024,
+        "lz": 1729.4144287109375,
+        "ux": 430359.0567932129,
+        "uy": 5087039.98236084,
+        "uz": 1944.4829025268555
+    },
+    "tightBoundingBox": {
+        "lx": 430143.988,
+        "ly": 5086824.914,
+        "lz": 1729.414,
+        "ux": 430359.057,
+        "uy": 5086974.32,
+        "uz": 1763.0430000000001
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 1.8625476360321046,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
